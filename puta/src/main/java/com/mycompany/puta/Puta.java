12c573a603eb1369b035d15f81ab5c61619a3b2a
jimenafdezz <153528628+jimenafdezz@users.noreply.github.com>
2024-02-23 17:54:57 +0100

    Update Puta.java

--- puta/src/main/java/com/mycompany/puta/Puta.java
+++ puta/src/main/java/com/mycompany/puta/Puta.java
@@ -11,6 +11,6 @@ package com.mycompany.puta;
 public class Puta {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("feo!");
     }
 }
