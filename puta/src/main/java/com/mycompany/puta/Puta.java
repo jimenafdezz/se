75f8bcfdf76411ba3692f8fc6d8574c1aeb70dbc
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-02-23 17:54:23 +0100

    sexo

---
+++ puta/src/main/java/com/mycompany/puta/Puta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.puta;
+
+/**
+ *
+ * @author DAW107
+ */
+public class Puta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
